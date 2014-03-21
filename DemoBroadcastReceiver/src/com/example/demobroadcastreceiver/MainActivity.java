cf4f90e5d0767922d0cd5eb2b8169a4d45cee5ac
khurram <khurram@khurram-developer>
2014-03-21 18:35:05 +0530

    test

---
+++ DemoBroadcastReceiver/src/com/example/demobroadcastreceiver/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demobroadcastreceiver;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Intent intent=new Intent();
+		
+		sendBroadcast(intent);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
